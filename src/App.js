978372e50f425dab3a4b1f4ca188a7d628c343d6
Hydration2O <bomalk121@gmail.com>
2024-11-14 17:36:11 +0300

    task1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import myimg from './assets/StorefrontV2.jpg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <h1>Cookies and Beyond</h1>
+        <p>Where cookie maniacs gather</p>
       </header>
+      <img className='bgimg' src={myimg} />
     </div>
   );
 }
