13a8373dd4a5606ba63e8bd259970efa2962f7c6
Hydration2O <bomalk121@gmail.com>
2024-11-14 20:06:42 +0300

    task2 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import "./App.css";
+import myimg from "./assets/StorefrontV2.jpg";
+import productData from "./products";
 
-import './App.css';
-import myimg from './assets/StorefrontV2.jpg'
+
 function App() {
   return (
     <div className="App">
-      <header >
+      <header>
         <h1>Cookies and Beyond</h1>
         <p>Where cookie maniacs gather</p>
       </header>
-      <img className='bgimg' src={myimg} />
+      <div>
+        <img className="bgImg" src={myimg} />
+      </div>
+       <div>
+      <div className="prodShelf">
+        <div className="prodItem">
+          <img className="prodImg" src={productData[0].img} />
+          <h3>{productData[0].name}</h3>
+          <p>{productData[0].price}</p>
+        </div>
+        <div className="prodItem">
+          <img className="prodImg" src={productData[1].img} />
+          <h3>{productData[1].name}</h3>
+          <p>{productData[1].price}</p>
+        </div>
+      </div>
     </div>
+     </div>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,16 @@
+import img1 from "./assets/ChocolateChipCookie.jpg"
+import img2 from "./assets/cookieMonsterCookie.jpeg"
+
+const productData=[
+  {
+    name: "choclate cookeie",
+    price:"5 KD",
+    img: img1,
+  },
+  {
+    name: "monster Cookie",
+    price: "1.5 KD",
+    img: img2,
+  }
+];
+export default productData;
